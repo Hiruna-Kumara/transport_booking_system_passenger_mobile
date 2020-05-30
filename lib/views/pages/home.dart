af92e43f7c2ac26c71a0501c1a74cce9b6d31420
Nilmani <nilmanikulaweera@gmail.com>
2020-05-30 08:57:54 +0530

    connect to api until view seat bookings

--- lib/views/pages/home.dart
+++ lib/views/pages/home.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:transport_booking_system_passenger_mobile/views/pages/auth.dart';
 import 'package:transport_booking_system_passenger_mobile/views/shared_widgets/page_widget.dart';
-import 'package:transport_booking_system_passenger_mobile/views/pages/bus_details.dart';
+import 'package:transport_booking_system_passenger_mobile/views/pages/route_details.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -12,12 +12,13 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   SharedPreferences sharedPreferences;
   String uid;
+  String token;
   bool _isLoading = true;
   final _formKey = GlobalKey<FormState>();
 
   String startingDestination = '';
   String endingDestination = '';
-  String journeyDate = '';
+  String journeyDate = DateTime.now().toString();
 
   DateTime selectedDate = DateTime.now();
 
@@ -31,6 +32,7 @@ class _HomeState extends State<Home> {
     if (picked != null && picked != selectedDate)
       setState(() {
         selectedDate = picked;
+        journeyDate = selectedDate.toString();
       });
   }
 
@@ -51,6 +53,7 @@ class _HomeState extends State<Home> {
         _isLoading = true;
       });
       uid = sharedPreferences.getString("uid");
+      token = sharedPreferences.getString("token");
       print(uid);
       setState(() {
         _isLoading = false;
@@ -151,11 +154,16 @@ class _HomeState extends State<Home> {
                             fontSize: 18.0,
                           ),
                         ),
-                        color: Colors.green[900],
+                        color: Colors.green[700],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5)
+                        ),
                         onPressed: () {
                           if (_formKey.currentState.validate()) {
                             Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => BusDetails(
+                              builder: (context) => RouteDetails(
+                                uid: uid,
+                                token: token,
                                 startingDestination: startingDestination,
                                 endingDestination: endingDestination,
                                 journeyDate: journeyDate,
---
+++ lib/views/pages/trip_details.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:transport_booking_system_passenger_mobile/controllers/authController.dart';
+import 'package:transport_booking_system_passenger_mobile/models/apiResponse.dart';
+import 'package:transport_booking_system_passenger_mobile/models/busTripData.dart';
+import 'package:transport_booking_system_passenger_mobile/views/pages/bus_layout_wrapper.dart';
+import 'package:intl/intl.dart';
+
+class TripDetails extends StatefulWidget {
+  final String uid;
+  final String token;
+  final String routeId;
+  TripDetails({this.uid, this.token, this.routeId});
+
+  @override
+  _TripDetailsState createState() => _TripDetailsState();
+}
+
+class _TripDetailsState extends State<TripDetails> {
+  final AuthController _auth = AuthController();
+  APIResponse<List<BusTripData>> _apiResponse;
+  bool _isLoading;
+  List<BusTripData> trips; 
+  String errorMessage;
+
+  @override
+  void initState() {
+    _fetchTripDetails();
+    super.initState();
+  }
+
+  _fetchTripDetails() async {
+    setState(() { _isLoading = true; });
+    _apiResponse = await _auth.getTurns(widget.routeId);
+    setState(() { 
+      if (_apiResponse.error){
+        _isLoading = false; 
+        errorMessage = _apiResponse.errorMessage;
+      } else {
+        _isLoading = false; 
+        trips = _apiResponse.data;
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[900],
+        title: Text(
+          'Trip Details',
+          style: TextStyle(
+            color: Colors.white
+          ),
+        )
+      ),
+      body: Container(
+        child: _isLoading? Center(child: CircularProgressIndicator()) : 
+        _apiResponse.error? Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 30, 0, 30),
+            child: Text(
+              "$errorMessage",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.green[900],
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ) : 
+        ListView.builder(
+          itemCount: trips.length,
+          itemBuilder: (context, index) {
+            return TripTile(uid:widget.uid, token:widget.token, trip: trips[index]);
+          }
+        ),
+      ),
+    );
+  }
+}
+
+class TripTile extends StatelessWidget {
+  final String uid;
+  final String token;
+  final BusTripData trip;
+  TripTile({this.uid, this.token, this.trip});
+
+  _formatDateTime(String dateTime) {
+    DateTime dt = DateTime.parse(dateTime);
+    dt = dt.add(Duration(hours: 5,minutes: 30));
+    String date = DateFormat.yMd().format(dt);
+    String time = DateFormat.jm().format(dt);
+    return ('$date at $time');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.grey[200],
+      margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 15.0),
+          ListTile(
+            leading: Icon(
+              Icons.airport_shuttle,
+              color: Colors.grey[700],
+            ),
+            title: Text(
+                  '${trip.startStation} to ${trip.endStation}',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+          ),
+          ListTile(
+            title: Text(
+              'Departure Date and Time',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            subtitle: Text(
+              _formatDateTime(trip.departureTime),
+              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'Arrival Date and Time',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            subtitle: Text(
+              _formatDateTime(trip.arrivalTime),
+              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'Bus Type',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            subtitle: Text(
+              'LKR ${trip.busType}',
+              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'Normal Seat Price',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            subtitle: Text(
+              'LKR ${trip.normalSeatPrice.toString()}',
+              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          FlatButton.icon( // to view the bookings of the current trip
+            label: Text(
+              'View Bookings',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5)
+            ),
+            color: Colors.green[700],
+            textColor: Colors.white,
+            icon: Icon(Icons.dashboard),
+            onPressed: () { 
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => BusLayoutWrapper(
+                  uid: uid, token: token, 
+                  tripId: trip.tripId, 
+                  seatPrice: trip.normalSeatPrice, 
+                  busType: trip.busType
+                )
+              ));   
+            },
+          ),
+          SizedBox(height: 15.0),
+        ],
+      ),
+    );
+  }             
+}
